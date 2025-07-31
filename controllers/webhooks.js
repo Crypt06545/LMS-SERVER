704fb7ce87bf28475d64d39897352d06a7df217d
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-07-31 09:57:29 +0600

    correction user created

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -15,7 +15,7 @@ export const clerkWebhooks = async (req, res) => {
       case "user.created": {
         const userData = {
           _id: data.id,
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           iamgeUrl: data.iamge_url,
         };
@@ -27,7 +27,7 @@ export const clerkWebhooks = async (req, res) => {
       case "user.updated": {
         const userData = {
           _id: data.id,
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           iamgeUrl: data.iamge_url,
         };
